10cd71ccd95651afb688e6ea356359cd91025431
EmranAhmed00 <emranahmed.my@gmail.com>
2019-11-12 21:40:09 +0100

    Add project files.

---
+++ AssignmentALM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AssignmentALM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
